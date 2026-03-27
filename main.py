bf67bc2f44d522d814a489cb826d545645f63c37
Bob Black <git@riblack.com>
2026-03-27 13:56:15 -0500

    logging_config

--- main.py
+++ main.py
@@ -72,11 +72,15 @@ def scrape() -> None:
         _prev_uncorrectables[cid] = ch.uncorrectables
 
 
-if __name__ == "__main__":
+def logging_config() -> None:
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s:%(levelname)s:%(name)s:%(message)s",
     )
+
+
+if __name__ == "__main__":
+    logging_config()
     logger.info("starting")
     start_http_server(8000)
     while True:

979bc6bca90bc315a360ce58ac44e57fd5d23db2
Bob Black <git@riblack.com>
2026-08-02 22:12:58 -0500

    test assert_attrs_list strip ansi sgr (#28)
    * strip ansi sgr in test__assert_attrs_list__*
    * more specific asserts

--- tests/test_test_shared.py
+++ tests/test_test_shared.py
@@ -1,8 +1,20 @@
+import logging
+import re
 from types import SimpleNamespace
 
 import pytest
 
 from .test_shared import assert_attrs_list
+
+logger = logging.getLogger(__name__)
+
+# ANSI SGR: CSI (\x1b[), digit/semicolon params, final byte "m".
+# Covers color, bold, and reset; not other CSI finals (cursor, erase).
+_ANSI_SGR_RE = re.compile(r"\x1b\[[0-9;]*m")
+
+
+def _strip_ansi_sgr(s: str) -> str:
+    return _ANSI_SGR_RE.sub("", s)
 
 
 def test__assert_attrs_list__empty():
@@ -34,7 +46,9 @@ def test__assert_attrs_list__objs_longer__shows_extra_obj_in_diff():
     with pytest.raises(AssertionError) as exc_info:
         assert_attrs_list(items, dict(a=1, b="x"))
 
-    assert "namespace(a=2, b='y')" in str(exc_info.value)
+    msg = _strip_ansi_sgr(str(exc_info.value))
+    logger.info("msg:\n%s\n-----\n%r", msg, msg)
+    assert "Left contains one more item: namespace(a=2, b='y')" in msg
 
 
 def test__assert_attrs_list__objs_shorter__shows_unmatched_expected_in_diff():
@@ -43,4 +57,6 @@ def test__assert_attrs_list__objs_shorter__shows_unmatched_expected_in_diff():
     with pytest.raises(AssertionError) as exc_info:
         assert_attrs_list(items, dict(a=1, b="x"), dict(a=2, b="y"))
 
-    assert "{'a': 2, 'b': 'y'}" in str(exc_info.value)
+    msg = _strip_ansi_sgr(str(exc_info.value))
+    logger.info("msg:\n%s\n-----\n%r", msg, msg)
+    assert "Right contains one more item: {'a': 2, 'b': 'y'}" in msg
